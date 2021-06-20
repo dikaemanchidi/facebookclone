a16e57ac50936b06e0f2c96bcf14aaabdca6d01d
dikaemanchidi <dikaemanchidi@gmail.com>
2021-06-20 23:09:39 +0900

    login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def new
+   end
+   def create
+     user = User.find_by(email: params[:session][:email].downcase)
+     if user && user.authenticate(params[:session][:password])
+       session[:user_id] = user.id
+       redirect_to user_path(user.id)
+     else
+       flash[:danger] = 'I failed to login'
+       render :new
+     end
+     def destroy
+       session.delete(:user_id)
+       flash[:notice] = 'logged out'
+       redirect_to new_session_path
+  end
+   end
+ end
